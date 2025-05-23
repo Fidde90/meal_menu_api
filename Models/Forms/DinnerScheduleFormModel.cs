2f88b791d88446b1ba43063ebb93082de42679f8
Fredrik <fredrik.t.bengtsson@hotmail.com>
2025-05-23 15:29:38 +0200

    i dont know..retard git

---
+++ Models/Forms/DinnerScheduleFormModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace meal_menu_api.Models.Forms
+{
+    public class DinnerScheduleFormModel
+    {
+        [Required]
+        public int NumberOfDays { get; set; }
+    }
+}
