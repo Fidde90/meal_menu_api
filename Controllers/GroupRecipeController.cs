78fd90537662c372f4dd71a47e827aaa4980e487
Fredrik <fredrik.t.bengtsson@hotmail.com>
2025-06-20 12:13:57 +0200

    added group-recipe controller

---
+++ Controllers/GroupRecipeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace meal_menu_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupRecipeController : ControllerBase
+    {
+    }
+}
